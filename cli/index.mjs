4453031bf003aeddbb1beae2a01f81ed357d159b
Clarence Manuel <rencedm112@gmail.com>
2022-05-22 12:08:16 +0800

    Refactored CLI

---
+++ cli/index.mjs
@@ -0,0 +1,4 @@
+import process from "process";
+import program from "./program.mjs";
+
+program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,9 @@ import {
 async function bootstrap() {
   const { AppModule } = await import("$app.module");
 
-  if (typeof process.env.NODE_ENV === "string") {
-    process.env.NEST_DEBUG = `${
-      process.env.NODE_ENV.toLowerCase() !== "production"
-    }`;
-  }
+  process.env.NEST_DEBUG = `${
+    process.env.NODE_ENV?.toLowerCase() !== "production"
+  }`;
 
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
